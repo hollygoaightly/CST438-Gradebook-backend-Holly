44f30eef77b65f6ed89e65de690235d1078b0a09
Holly <hstephens@csumb.edu>
2021-10-12 21:27:43 -0700

    Assignment 6 student / instructor access logic

--- cst438_gradebook/src/main/java/com/cst438/domain/EnrollmentRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/EnrollmentRepository.java
@@ -1,7 +1,12 @@
 package com.cst438.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EnrollmentRepository extends CrudRepository <Enrollment, Integer> {
+	
+	@Query("select a from Enrollment a where a.studentEmail = :studentEmail")
+	Enrollment findByEmail(@Param("studentEmail") String studentEmail);
 
 }
